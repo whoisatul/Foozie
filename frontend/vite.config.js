4c8b5b04affc05e55406588293d3aaf45f963755
whoisatul <160370790+whoisatul@users.noreply.github.com>
2025-06-11 16:14:11 +0530

    backend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      'api' : 'http://localhost:3000'
+    }
+  },
   plugins: [react(),tailwindcss()],
 })
